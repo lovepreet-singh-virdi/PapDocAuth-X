b6777bb53ceb8b5e18c1fba427f5c6427e3c4df2
lovepreet-singh-virdi <lovepreetsinghvirdi001@gmail.com>
2025-11-17 23:06:46 -0500

    chore: add service index aggregator for cleaner imports

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+export * from "./hashingService.js";
+
+// Later we will add:
+// export * from "./authService.js";
+// export * from "./documentService.js";
+// export * from "./verificationService.js";
+// export * from "./workflowService.js";
+// export * from "./auditService.js";
+// export * from "./qrService.js";
